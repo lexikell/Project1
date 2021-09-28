321096e5216d2ba8fae758bcbcc6f5f02987b155
lexikell <87827187+lexikell@users.noreply.github.com>
2021-09-27 22:45:40 -0400

    Update blacklist.sh

--- blacklist.sh
+++ blacklist.sh
@@ -2,10 +2,10 @@
 #SBATCH --account=def-juniacke
 #SBATCH --job-name=blacklist
 #SBATCH --ntasks-per-node=8
-#SBATCH --time=0-06:00:00
-#SBATCH --mem-per-cpu=15G
-#SBATCH --output=BwaMem.%J.out
-#SBATCH --error=BwaMem.%J.err
+#SBATCH --time=0-00:10:00
+#SBATCH --mem-per-cpu=1G
+#SBATCH --output=Blacklist.%J.out
+#SBATCH --error=Blacklist.%J.err
 
 module load StdEnv/2020
 module load nixpkgs/16.09  gcc/7.3.0
@@ -18,7 +18,7 @@ module load bedtools/2.29.2
 
 ID="18Sept21"
 WORKDIR=/home/akelling/projects/def-juniacke/akelling/Data1/2I7I3XE/KEL17000.20210603/210602_A00481_0206_AHFM2CDRXY/18Sept21/macs2
-SAMPLE="PAbJUL1"
+SAMPLE="PAbJUL3"
 
 blacklist=/home/akelling/projects/def-juniacke/akelling/Data1/2I7I3XE/KEL17000.20210603/210602_A00481_0206_AHFM2CDRXY/blacklist/hg38-blacklist.bed
 sample=$WORKDIR/$SAMPLE"_MACS2broad_"$ID"_peaks.broadPeak"
