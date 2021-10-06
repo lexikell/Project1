46905c5401f684aec45aa6e67814d00ef9813ab0
lexikell <87827187+lexikell@users.noreply.github.com>
2021-10-05 23:57:44 -0400

    Create dt_computematrix.sh

---
+++ dt_computematrix.sh
@@ -0,0 +1,7 @@
+
+
+
+
+#download bam gene list from UCSC (not shown here)
+#move that gzip to computecan:
+scp /Users/lexikellington/seq/UCSCgenes.bed.gz akelling@cedar.computecanada.ca:/home/akelling/projects/def-juniacke/akelling/Data1/2I7I3XE/KEL17000.20210603/210602_A00481_0206_AHFM2CDRXY/18Sept21/deeptools
