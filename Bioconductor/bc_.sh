c0309472ddb70465504c430fd9cba41efcb29db4
lexikell <87827187+lexikell@users.noreply.github.com>
2021-10-29 22:13:27 -0400

    Update bc_.sh

--- Bioconductor/bc_.sh
+++ Bioconductor/bc_.sh
@@ -2,10 +2,11 @@
 #SBATCH --account=def-juniacke
 #SBATCH --job-name=bc_
 #SBATCH --ntasks=15
-#SBATCH --time=0-14:00:00
+#SBATCH --time=0-14:00:00   # time (DD-HH:MM)
 #SBATCH --mem-per-cpu=40G
 #SBATCH --output=bc_.%J.out
 #SBATCH --error=bc_.%J.err
+
 
 module load StdEnv/2020 gcc/9.3.0
 module load r/4.1.0
@@ -17,4 +18,6 @@ module load r-bundle-bioconductor/3.12
 #to run R in your linux terminal:
 # > "R"
 #now you can use R language. To quit "q()"
+
+#to run R script in slurm job, use function "Rscript *.R" to run whatever script
 
