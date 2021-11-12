80cd9cb07a616274b1a41b22a905908378866516
lexikell <87827187+lexikell@users.noreply.github.com>
2021-11-11 22:38:40 -0500

    Update bc_ChIPpeakanno_compare.R

--- Bioconductor/bc_ChIPpeakanno_compare.R
+++ Bioconductor/bc_ChIPpeakanno_compare.R
@@ -71,9 +71,92 @@ upTSSs100_8.gr[28, ] -> promoter
 data.frame(promoter)
 grep("59686445", upTSSs100_8.broadpeak) #output:4 THIS IS NOT THE ROW IDK WHAT THIS IS
 upTSSs100_8.broadpeak[2, ]
+#outputs:
+#59686445-59687485
+#upTSSs100_5.gr__X088
+#PAbJUL5_MACS2broad_18Sept21_peak_33656
 
+#NEW DATASET:
+#see overlapping peaks of only peaks -8kb--1 TSS
+#1st: 
+#make a new df from annoIDsNdf_upTSS8kb keeping only the columns for Broadpeak format
+annoIDsNdf_upTSS8kb %>% select(seqnames, start, end, peak, score, strand, signalValue, pValue, qValue) -> upTSS8kb_N.broadpeak
+upTSS8kb_N.broadpeak %>% 
+  rename(
+    chrom = seqnames,
+    chromStart = start,
+    chromEnd = end,
+    name = peak,
+  )
+annoIDsHdf_upTSS8kb %>% select(seqnames, start, end, peak, score, strand, signalValue, pValue, qValue) -> upTSS8kb_H.broadpeak
+upTSS8kb_H.broadpeak %>% 
+  rename(
+    chrom = seqnames,
+    chromStart = start,
+    chromEnd = end,
+    name = peak,
+  )
+annoIDs8df_upTSS8kb %>% select(seqnames, start, end, peak, score, strand, signalValue, pValue, qValue) -> upTSS8kb_8.broadpeak
+upTSS8kb_8.broadpeak %>% 
+  rename(
+    chrom = seqnames,
+    chromStart = start,
+    chromEnd = end,
+    name = peak,
+  )
+annoIDs5df_upTSS8kb %>% select(seqnames, start, end, peak, score, strand, signalValue, pValue, qValue) -> upTSS8kb_5.broadpeak
+upTSS8kb_5.broadpeak %>% 
+  rename(
+    chrom = seqnames,
+    chromStart = start,
+    chromEnd = end,
+    name = peak,
+  )
 
-59686445-59687485
-upTSSs100_5.gr__X088
-PAbJUL5_MACS2broad_18Sept21_peak_33656
+#2nd: turn those df into grange files using toGRanges
+#make GRanges files of all
+upTSS8kb_Ngr <- toGRanges(upTSS8kb_N.broadpeak, format="broadPeak")
+upTSS8kb_Hgr <- toGRanges(upTSS8kb_H.broadpeak, format="broadPeak")
+upTSS8kb_8gr <- toGRanges(upTSS8kb_8.broadpeak, format="broadPeak")
+upTSS8kb_5gr <- toGRanges(upTSS8kb_5.broadpeak, format="broadPeak")
 
+#3rd
+#Find overlapping peaks and save files
+#ERROR keeps occurring with findOverlapsOfPeaks here:
+ol_NHP_upTSS8kb <- findOverlapsOfPeaks(upTSS8kb_Ngr, upTSS8kb_Hgr, upTSS8kb_8gr, upTSS8kb_5gr)
+#ERROR: "Inputs contains duplicated ranges"
+#remove N and it works...
+ol_NHP_upTSS8kb <- findOverlapsOfPeaks(upTSS8kb_Hgr, upTSS8kb_8gr, upTSS8kb_5gr)
+#ONLINE FORUM HELP!:
+#duplicates will make this error. To check for duplicates: 
+df <- data.frame(upTSS8kb_Ngr)
+df[duplicated(df), ]
+# To remove duplicates, simply:
+upTSS8kb_Ngr2 <- unique(upTSS8kb_Ngr)
+#and now try with new gr:
+ol_NHP_upTSS8kb <- findOverlapsOfPeaks(upTSS8kb_Ngr2, upTSS8kb_Hgr, upTSS8kb_8gr, upTSS8kb_5gr)
+#IT WORKED! NO ERROR! ON WITH THE SHOW!:
+ol_NHP_upTSS8kb$overlappingPeaks -> NHP_upTSS8kb_olpeaks
+ol_NHP_upTSS8kb$peaklist -> NHP_upTSS8kb_peaks
+
+#4th: venn diagram plot
+makeVennDiagram(ol_NHP_upTSS8kb,
+                NameOfPeaks=c("Normoxia", "Hypoxia", "Physioxia8", "Physioxia5"),
+                fill=c(3,2,5,4),
+                main = "Overlapping Peaks 8Kb upstream of TSS in Normoxia, Physioxia, and Hypoxia")
+#save venn diagram counts
+ol_NHP_upTSS8kb$venn_cnt -> VennCountNHP_upTSS8kb
+#save overlapped peaks venn count as a csv
+write.csv(VennCountNHP_upTSS8kb, file = "VennCountNHPupTSS8kb_10Nov21.csv")
+#annotate
+#show peaks in common bt two names: 
+NHP_upTSSs100_peaks[["upTSSs100_8.gr///upTSSs100_5.gr"]]
+#then locate the common one in the olpeaks to get peak name to search file for gene name
+NHP_upTSSs100_peaks[["upTSSs100_8.gr///upTSSs100_5.gr"]]
+#search peak name in gene list of that sample
+grep("59686445", upTSSs100_8.gr)  #output: 28
+
+upTSSs100_8.gr[28, ] -> promoter
+data.frame(promoter)
+grep("59686445", upTSSs100_8.broadpeak) #output:4 THIS IS NOT THE ROW IDK WHAT THIS IS
+upTSSs100_8.broadpeak[2, ]
