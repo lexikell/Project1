c64d0538a47211133500bb58a1dbd569eef8ffcf
lexikell <87827187+lexikell@users.noreply.github.com>
2022-02-23 21:38:46 -0500

    Create Download.sh

---
+++ Download.sh
@@ -0,0 +1,5 @@
+#File download and check-up when you first download a file 
+#download to whatever folder 
+curl -o 111021AKellington.zip https://adcprdgenpln.blob.core.windows.net/juniacke/111021Akellington.zip
+
+#Md5 is the "fingerprint" of that file. You should request and check this 
