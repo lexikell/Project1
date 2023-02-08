b278b5d8369716356ec75e95f9a29e64731c6aa6
lexikell <lexi.kellington@gmail.com>
2023-02-08 16:23:30 -0500

    Update dt_multibamsumOther.sh

--- deeptools/dt_multibamsumOther.sh
+++ deeptools/dt_multibamsumOther.sh
@@ -2,8 +2,8 @@
 #SBATCH --account=def-juniacke
 #SBATCH --job-name=dt_bamsumother
 #SBATCH --ntasks-per-node=8
-#SBATCH --time=0-02:00:00
-#SBATCH --mem-per-cpu=2G
+#SBATCH --time=0-04:00:00
+#SBATCH --mem-per-cpu=3G
 #SBATCH --output=dt_bamsum.%J.out
 #SBATCH --error=dt_bamsum.%J.err
 
@@ -12,7 +12,7 @@ module load gcc/9.3.0
 module load python/3.9
 module load scipy-stack/2021a
 module load imkl/2020.1.217
-module load nixpkgs/16.09  intel/2018.3
+
 pip install plotly
 pip install deeptools
 
@@ -41,5 +41,4 @@ multiBamSummary bins --bamfiles $C1 $S1 $S2 $S3 $S4 $S7 --labels IgG Normoxia Hy
 plotCorrelation -in $output/$name.npz --corMethod spearman --skipZeros --plotTitle "Spearman Correlation of Read Counts" --whatToPlot heatmap --colorMap coolwarm --plotNumbers -o $output/heatmap_$name.png --outFileCorMatrix $output/SpearmanCorr_$name.tab
 
 #onto local to visualize
-#scp akelling@cedar.computecanada.ca:/home/akelling/projects/def-juniacke/akelling/Data1/2I7I3XE/KEL17000.20210603/210602_A00481_0206_AHFM2CDRXY/18Sept21/deeptools/heatmap_multibwSummary_all_genes18Sept21.png /Users/lexikellington/seq/deeptools
-#53345261
+#scp akelling@cedar.computecanada.ca:/home/akelling/projects/def-juniacke/akelling/Data1/2I7I3XE/KEL17000.20210603/210602_A00481_0206_AHFM2CDRXY/18Sept21/deeptools/other/heatmap_multibamSum_OGG1.png /Users/lexikellington/seq/deeptools
