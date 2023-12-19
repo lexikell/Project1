4839890a2e9d0c0cc7b4a2226f29656ef22b320e
lexikell <lexi.kellington@gmail.com>
2023-12-19 13:59:58 -0500

    Update dt_promoterheatmapPLANA.sh

--- deeptools/dt_promoterheatmapPLANA.sh
+++ deeptools/dt_promoterheatmapPLANA.sh
@@ -30,10 +30,14 @@ C4=$WORKDIR/"CAbJUL8""_""$ID".bam
 blacklist=/home/akelling/projects/def-juniacke/akelling/Data1/2I7I3XE/KEL17000.20210603/210602_A00481_0206_AHFM2CDRXY/blacklist/hg38-blacklist.bed
 output=/home/akelling/projects/def-juniacke/akelling/Data1/2I7I3XE/KEL17000.20210603/210602_A00481_0206_AHFM2CDRXY/18Sept21/deeptools
 name="promoterheatmapPLANA"
+genes=/home/akelling/projects/def-juniacke/akelling/Data1/2I7I3XE/KEL17000.20210603/210602_A00481_0206_AHFM2CDRXY/18Sept21/deeptools/UCSCgenes.bed
 
 NBAM=/home/akelling/projects/def-juniacke/akelling/project_202107/30_bioconductor/rep1/chippeakanno/annoIDsNdf_up5000down1000.bam
 5BAM=/home/akelling/projects/def-juniacke/akelling/project_202107/30_bioconductor/rep1/chippeakanno/annoIDs5df_up5000down1000.bam
 HBAM=/home/akelling/projects/def-juniacke/akelling/project_202107/30_bioconductor/rep1/chippeakanno/annoIDsHdf_up5000down1000.bam
+
+#PLAN A
+#let's trim our bw to just our promoter regions so that we can use kmeans and normal genes to compare 
 
 #turn the BAMs into bigwigs comparing to the control using bamcompare 
 bamCompare -b1 $S4 -b2 $C4 --scaleFactorsMethod readCount -bl $blacklist -o $name.bw -of bigwig
